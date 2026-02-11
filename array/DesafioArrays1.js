ef260dbc7fcb22cde9c2109873aafe06dccc0302
carreiraandre21-art <carreiraandre21@gmail.com>
2026-02-11 14:58:11 -0300

    DesafiosArrays

--- array/DesafioArrays1.js
+++ array/DesafioArrays1.js
@@ -43,7 +43,7 @@ numeros.forEach
         }
 )
 console.log("A soma dos números é: " + soma);
-*/
+
 
 
 
@@ -64,6 +64,8 @@ console.log(multiplosDe3. join(" - ") + " é múltiplo de 3");
 
 //--------------------------------------------------------------------------------------------------------------------------------
 
+//sem usar o filter
+
 let array = [];
 let errados = [];
 
@@ -82,3 +84,44 @@ multiplos.forEach
 
 console.log(array.join(" - ") + " é múltiplo de 3");
 console.log(errados.join(" - ") + " não é múltiplo de 3");
+
+*/ 
+
+
+// fitrar tamanhos de palavras com mais de 5 letras'
+
+const palavras = ["gato", "cachorro", "computador", "elefante", "telefone", "peixe", "leao", "floresta", "bicicleta"];
+
+let palavrasComMaisDe5Letras = [];
+let palavrasCom5LetrasOuMenos = [];
+
+palavras.forEach 
+( 
+    function (palavra) 
+    {
+        if (palavra.length > 5 )
+         {
+            palavrasComMaisDe5Letras.push(palavra);
+         }
+         else 
+            palavrasCom5LetrasOuMenos.push(palavra);
+    }
+)
+
+console.log("Palavras com mais de 5 letras: " + palavrasComMaisDe5Letras.join(")-("));
+console.log("Palavras com 5 letras ou menos: " + palavrasCom5LetrasOuMenos.join(")-("));
+
+
+
+
+// cm filter
+
+const palavrasComMaisDe5Letras2 = palavras.filter(function (palavra) {
+    return palavra.length > 5;
+});
+const palavrasCom5LetrasOuMenos2 = palavras.filter(function (palavra) {
+    return palavra.length <= 5;
+});
+
+console.log("FILTER Palavras com mais de 5 letras: " + palavrasComMaisDe5Letras2.join(")-("));
+console.log("FILTER Palavras com 5 letras ou menos: " + palavrasCom5LetrasOuMenos2.join(")-("));
